597d71ca65471860554fe822018bb6bccc97c3c6
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-09-23 18:11:06 +0200

    Created global navigation

---
+++ src/app/components/nav.js
@@ -0,0 +1,15 @@
+"use client"
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export default function Navigation() {
+    const pathName = usePathname();
+
+    return (
+        <nav className='w-full p-4 border-b border-b-gray-400 flex justify-center'>
+          <Link href="/pokemon" className={`mx-2 text-gray-500 transition-colors hover:text-black ${pathName === '/pokemon' ? 'active' : ''}`}>Pokemons</Link>
+          <Link href="/types" className={`mx-2 text-gray-500 transition-colors hover:text-black ${pathName === '/types' ? 'active' : ''}`}>Types</Link>
+        </nav>
+    )
+}
