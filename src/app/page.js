aacac6929621c2e42b3ab1a6857290d40a564c04
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-09-23 22:46:16 +0200

    text update

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/pokemon">Pokémon</Link>
+      <p>This site is created for the purpose of playing around with Next.js and Sanity.</p>
     </main>
   )
 }
