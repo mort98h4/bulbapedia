664a9768a354807a62be9bb2d8402c3a6abae1d5
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-09-23 19:38:22 +0200

    logic to not display nav on sanity studio page

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -7,9 +7,16 @@ export default function Navigation() {
     const pathName = usePathname();
 
     return (
-        <nav className='w-full p-4 border-b border-b-gray-400 flex justify-center'>
-          <Link href="/pokemon" className={`mx-2 text-gray-500 transition-colors hover:text-black ${pathName === '/pokemon' ? 'active' : ''}`}>Pokemons</Link>
-          <Link href="/types" className={`mx-2 text-gray-500 transition-colors hover:text-black ${pathName === '/types' ? 'active' : ''}`}>Types</Link>
-        </nav>
+      <>
+        {
+          pathName.includes('/studio') ?
+            ""
+          : 
+          <nav className='w-full p-4 border-b border-b-gray-400 flex justify-center'>
+            <Link href="/pokemon" className={`mx-2 text-gray-500 transition-colors hover:text-black ${pathName === '/pokemon' ? 'active' : ''}`}>Pokemons</Link>
+            <Link href="/types" className={`mx-2 text-gray-500 transition-colors hover:text-black ${pathName === '/types' ? 'active' : ''}`}>Types</Link>
+          </nav>
+        }
+      </>
     )
 }
