68638901eac844fa04a38d3670245a5cf4402cdb
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-09-23 22:46:39 +0200

    Added some text

--- src/app/pokemon/page.js
+++ src/app/pokemon/page.js
@@ -6,6 +6,9 @@ export default function Page() {
     return (
         <section className="grid gap-4">
             <Header title="Pokémon"/>
+            <p>
+                <strong>Pokémon</strong> (Japanese: ポケットモンスター <em>Pocket Monsters</em>; ポケモン <em>Pokémon</em> for short) are fictional creatures that are central to the Pokémon franchise. 
+            </p>
             <PokemonList />
         </section>
     )
